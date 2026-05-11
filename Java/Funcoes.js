94cccd7ab00657a10dd4fb95f9340da3827ce008
cunha-rafael <rcunhabh.23@gmail.com>
2026-05-11 16:09:35 -0300

    Teste de funcoes

---
+++ Java/Funcoes.js
@@ -0,0 +1,50 @@
+/*
+function imprimir() {
+   console.log("Olá Mundo");
+
+  }
+
+imprimir(); 
+imprimir();
+imprimir();
+
+
+function calcularMultiplicao() {
+   console.log(25 * 57);
+}
+
+calcularMultiplicao();
+
+
+function calcularMultiplicao(valor1 , valor2) {
+   console.log(`A multiplicao entre ${valor1} e ${valor2} é: ${valor1 * valor2}`);
+
+}
+
+calcularMultiplicao(25, 57);
+calcularMultiplicao(10, 38);
+calcularMultiplicao(13, 97);
+calcularMultiplicao(65, 87);
+
+function imprimir(impressao) {
+  console.log(impressao);
+
+}
+
+imprimir("Olá Mundo!");
+*/
+
+/*const imprimir = function imprimir(impressao){
+   console.log(impressao);
+
+}
+ 
+imprimir("Hello World");
+*/
+
+const imprimir = function(impressao){
+   console.log(impressao);
+
+}
+ 
+imprimir("Hello World");
