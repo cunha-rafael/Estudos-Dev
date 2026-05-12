40eed7da14fdedaf68b207dae9227e3201292ee7
cunha-rafael <rcunhabh.23@gmail.com>
2026-05-12 10:31:15 -0300

    Arrays

---
+++ Java/Arrays.js
@@ -0,0 +1,4 @@
+const listaDoCasamento = ["Diva", "Flavia", "Ana", "Amanda", "Nubia", "Nagila", "Carlos", "Aline"];
+
+console.log("Vou casar e os convidados são:", listaDoCasamento);
+console.log("Quantas pessoas convidei?", listaDoCasamento.length);
