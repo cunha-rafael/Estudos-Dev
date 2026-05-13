35663da61e931608c0d750e515c07a156b5fac68
cunha-rafael <rcunhabh.23@gmail.com>
2026-05-13 09:44:47 -0300

    Manipulando arrays

--- Java/Arrays.js
+++ Java/Arrays.js
@@ -2,3 +2,19 @@ const listaDoCasamento = ["Diva", "Flavia", "Ana", "Amanda", "Nubia", "Nagila", 
 
 console.log("Vou casar e os convidados são:", listaDoCasamento);
 console.log("Quantas pessoas convidei?", listaDoCasamento.length);
+
+console.log("Convidado numero 1:", listaDoCasamento[0]);
+console.log("Convidado numero 8:", listaDoCasamento[listaDoCasamento.length - 1]);
+
+console.log("Nubia esta em qual posicao do array?", listaDoCasamento.indexOf("Nubia"));
+
+console.log(typeof listaDoCasamento);
+
+listaDoCasamento.push("Julia");
+listaDoCasamento.pop();
+
+
+listaDoCasamento.unshift("Rafael");
+listaDoCasamento.shift();
+
+console.log("Nagila esta na lista?:", listaDoCasamento.includes("Nagila"));
