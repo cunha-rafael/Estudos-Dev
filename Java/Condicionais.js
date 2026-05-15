8c686013b2a57ea449bb6c07c6ad7a66abbf87be
cunha-rafael <rcunhabh.23@gmail.com>
2026-05-15 10:49:35 -0300

    Condicionais If e Else

---
+++ Java/Condicionais.js
@@ -0,0 +1,40 @@
+// const idadeMaria = 16;
+// const maioridade = 18;
+
+// if (idadeMaria >= 18) {
+//    console.log("Maria pode dirigir!");
+// } else {
+//    const anosFaltantes = maioridade - idadeMaria;
+//    console.log("Maria não pode dirigir!");
+//    console.log(`Faltam ${anosFaltantes} anos para Maria poder dirigir.`);
+// }
+
+// const idadePessoa = 26;
+
+// 0 a 12 anos -> infancia
+// 13 a 17 anos -> adolescencia 
+// 18 a 25 anos -> jovem adulto 
+// 25 a 60 anos -> adulto
+// mais que 60 anos -> idoso 
+
+// if (idadePessoa >= 0 && idadePessoa <= 12) {
+//   console.log("Infância");
+// } else if (idadePessoa >= 13 && idadePessoa <= 17) {
+//   console.log("Adolescência");
+// }  else if (idadePessoa >= 18 && idadePessoa <= 25) {
+//   console.log("Jovem Adulto");
+// }  else if (idadePessoa >= 26 && idadePessoa <= 60) {
+//   console.log("Adulto");
+// } else {
+//   console.log("Idoso");
+// }
+
+const x = 20;
+
+if (x % 2 === 0) {
+   console.log(`o número ${x} é par`);
+}
+
+if (x % 5 === 0 ) {
+   console.log(`O número ${x} é múltiplo de 5`);
+}
